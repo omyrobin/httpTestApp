cd3b3a00b8984eedb1047681f9c1ecf7b80f69a2
Omyrobin <omy_robin@126.com>
2020-04-28 11:03:00 +0800

    添加application

--- app/src/main/java/com/omyrobin/httptestapp/MainActivity.java
+++ app/src/main/java/com/omyrobin/httptestapp/MainActivity.java
@@ -1,19 +1,16 @@
 package com.omyrobin.httptestapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.monkey.network.NetWorkManager;
 import com.monkey.network.rxjava.Callback;
+import com.monkey.network.rxjava.tranformer.DataTranformer;
 import com.omyrobin.httptestapp.api.ApiService;
 import com.omyrobin.httptestapp.bean.TestBean;
 import com.omyrobin.httptestapp.rxjava.BaseTranformer;
-
-import io.reactivex.disposables.CompositeDisposable;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Consumer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,24 +18,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getMethod();
     }
 
 
     @SuppressLint("CheckResult")
     private void getMethod(){
-        final CompositeDisposable compositeDisposable = new CompositeDisposable();
         ApiService apiService = NetWorkManager.getInstance().createService(ApiService.class);
+
         apiService.getList()
                 .compose(new BaseTranformer<TestBean>())
-                .doOnSubscribe(new Consumer<Disposable>() {
-                    @Override
-                    public void accept(Disposable disposable) throws Exception {
-                        compositeDisposable.add(disposable);
-                    }
-                })
                 .subscribe(new Callback<TestBean>() {
                     @Override
                     public void onSuccess(TestBean testBean) {
+
+                    }
+
+                    @Override
+                    public void onFailure(Throwable throwable) {
+
+                    }
+                });
+
+
+        apiService.postList("123")
+                .compose(new DataTranformer<String>())
+                .subscribe(new Callback<String>() {
+                    @Override
+                    public void onSuccess(String s) {
 
                     }
 
